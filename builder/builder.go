50a80d3fd78f1862ab1d0a3d3fe02838dc62a9a4
Quentin Renard <contact@asticode.com>
2017-03-25 12:47:03 +0100

    Added builder

---
+++ builder/builder.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/rs/xlog"
+)
+
+// Constants
+const (
+	keyBits = 4096
+)
+
+// Builder represents a builder
+type Builder struct {
+	logger          xlog.Logger
+	rootProjectPath string
+}
+
+// NewBuilder returns a new builder
+func NewBuilder(c Configuration, l xlog.Logger) *Builder {
+	l.Debug("Starting builder")
+	return &Builder{
+		logger:          l,
+		rootProjectPath: c.RootProjectPath,
+	}
+}
+
+// Close closes the builder
+func (b *Builder) Close() {
+	b.logger.Debug("Stopping client")
+}
+
+// GenerateKey generates an rsa key with an optional passphrase
+func (b *Builder) GenerateKey(passphrase string) (k []byte, err error) {
+	// Generate RSA key
+	var pk *rsa.PrivateKey
+	if pk, err = rsa.GenerateKey(rand.Reader, keyBits); err != nil {
+		return
+	}
+
+	// Convert it to pem
+	var block = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(pk),
+	}
+
+	// Encrypt the pem
+	if len(passphrase) > 0 {
+		if block, err = x509.EncryptPEMBlock(rand.Reader, block.Type, block.Bytes, []byte(passphrase), x509.PEMCipherAES256); err != nil {
+			return
+		}
+	}
+
+	// Encode to memory
+	k = pem.EncodeToMemory(block)
+	return
+}
+
+// Build builds the client
+func (b *Builder) Build(outputPath, outputOS string, privateKey []byte) (err error) {
+	// Retrieve git version
+	var v []byte
+	if v, err = b.GitVersion(); err != nil {
+		return
+	}
+
+	// Build
+	var ldflags = fmt.Sprintf("-X main.PrivateKey=%s -X main.Version=%s", base64.StdEncoding.EncodeToString(privateKey), v)
+	var cmd = exec.Command("go", "build", "-o", fmt.Sprintf("%s/client/client", outputPath), "-ldflags", ldflags, fmt.Sprintf("%s/client", b.rootProjectPath))
+	cmd.Env = b.buildEnv(outputOS)
+	b.logger.Debugf("Running %s", strings.Join(append(cmd.Env, cmd.Args...), " "))
+	var o []byte
+	if o, err = cmd.CombinedOutput(); err != nil {
+		err = fmt.Errorf("%s: %s", err, string(o))
+		return
+	}
+	return
+}
+
+// buildEnv returns the build environment variables
+func (b *Builder) buildEnv(outputOS string) []string {
+	return []string{"GOOS=linux", "GOARCH=amd64", "GOPATH=" + os.Getenv("GOPATH")}
+}
+
+// GitVersion retrieves the project's git version
+func (b *Builder) GitVersion() (o []byte, err error) {
+	var cmd = exec.Command("git", "--git-dir", fmt.Sprintf("%s/.git", b.rootProjectPath), "rev-parse", "HEAD")
+	b.logger.Debugf("Running %s", strings.Join(cmd.Args, " "))
+	if o, err = cmd.CombinedOutput(); err != nil {
+		return
+	}
+	o = bytes.TrimSpace(o)
+	return
+}
---
+++ builder/configuration.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/BurntSushi/toml"
+	"github.com/asticode/go-astilog"
+	"github.com/imdario/mergo"
+	"github.com/rs/xlog"
+)
+
+// Flags
+var (
+	configPath      = flag.String("c", "", "the config path")
+	rootProjectPath = flag.String("r", "", "the root project path")
+)
+
+// Configuration represents a configuration
+type Configuration struct {
+	Logger          astilog.Configuration `toml:"logger"`
+	RootProjectPath string                `toml:"root_project_path"`
+}
+
+// TOMLDecodeFile allows testing functions using it
+var TOMLDecodeFile = func(fpath string, v interface{}) (toml.MetaData, error) {
+	return toml.DecodeFile(fpath, v)
+}
+
+// NewConfiguration creates a new configuration object
+func NewConfiguration() Configuration {
+	// Global config
+	gc := Configuration{
+		Logger: astilog.Configuration{
+			AppName: "go-astichat-builder",
+		},
+		RootProjectPath: ".",
+	}
+
+	// Local config
+	if *configPath != "" {
+		// Decode local config
+		if _, err := TOMLDecodeFile(*configPath, &gc); err != nil {
+			xlog.Fatalf("%v while decoding the config path %s", err, *configPath)
+		}
+	}
+
+	// Flag config
+	c := Configuration{
+		Logger:          astilog.FlagConfig(),
+		RootProjectPath: *rootProjectPath,
+	}
+
+	// Merge configs
+	if err := mergo.Merge(&c, gc); err != nil {
+		xlog.Fatalf("%v while merging configs", err)
+	}
+
+	// Return
+	return c
+}
